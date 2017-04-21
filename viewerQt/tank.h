2d3bf1ad30c963b244d0a1ddd3f323614b58d4c1
77freab <77freab@gmail.com>
2017-04-21 15:48:42 +0400

    добавил анимацию уничтожения танка

--- viewerQt/tank.h
+++ viewerQt/tank.h
@@ -29,6 +29,31 @@ public:
   void operator()(osg::Node*, osg::NodeVisitor*);
 };
 
+class bangCallback : public osg::NodeCallback
+{
+public:
+  void operator()(osg::Node*, osg::NodeVisitor*);
+};
+
+class bang : public osg::MatrixTransform
+{
+public:
+  bang(int x, int y, int z, std::list<osg::Node*>* toDelete);
+  void makeBang();
+private:
+  osg::ref_ptr<osg::Geode> _geode;
+  osg::ref_ptr<osg::Geometry> _geom;
+  osg::ref_ptr<osg::Vec4Array> _color;
+  osg::ref_ptr<osg::Vec3Array> _vertices;
+  osg::ref_ptr<osg::Vec3Array> _normals;
+  osg::ref_ptr<osg::Vec2Array> _texCoord;
+  osg::ref_ptr<osg::Image> _image;
+  osg::ref_ptr<osg::Texture2D> _texture;
+  int _roughTimer = 0;
+  std::list<osg::Node*>* _toDelete;
+  osg::ref_ptr<bangCallback> _clb;
+};
+
 class projectile;
 
 class tank : public QObject, public osg::MatrixTransform
